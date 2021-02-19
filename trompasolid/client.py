241e20b179617cb4f7eeea039dee0b515c1656e3
Alastair Porter <alastair.porter@upf.edu>
2021-02-19 16:54:02 +0100

    Add redis configuration

--- trompasolid/client.py
+++ trompasolid/client.py
@@ -6,15 +6,24 @@ import redis
 from jwcrypto import jwk
 
 
+_redis = None
+
+
+def init_redis(host="localhost", port=6379):
+    global _redis
+    _redis = redis.Redis(host=host, port=port)
+
+
 def get_bearer_for_user(provider, user):
     """Given a solid provider, and a user vcard, get the bearer token needed
     to write to this provider as the user."""
-    r = redis.Redis()
+    if _redis is None:
+        raise ValueError("Must call init_redis first")
 
     token_r_key = f"solidauth-rs-token-{provider}-{user}"
-    id_token = r.get(token_r_key).decode("utf-8")
+    id_token = _redis.get(token_r_key).decode("utf-8")
     jwk_r_key = "solidauth-local-key"
-    local_jwk = r.get(jwk_r_key)
+    local_jwk = _redis.get(jwk_r_key)
     private_key = jwk.JWK.from_json(local_jwk)
 
     # TODO: Check that the signature is signed with the pubkey of the above private key
@@ -34,7 +43,6 @@ def get_bearer_for_user(provider, user):
         "token_type": "pop",
         "jti": str(uuid.uuid4())
     }
-    print(pop_token)
 
     bearer = jwt.encode(pop_token, key=private_key.export_to_pem(True, None), algorithm="RS256")
 
