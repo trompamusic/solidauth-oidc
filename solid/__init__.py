c5620423c41069641a9061affc8a8f7e90c553ae
Alastair Porter <alastair@porter.net.nz>
2023-02-06 16:47:03 +0100

    Use PKCE, client_id registration

--- solid/__init__.py
+++ solid/__init__.py
@@ -1,12 +1,22 @@
-import json
-import secrets
+import base64
+import datetime
+import hashlib
+import os
+import re
+import urllib.parse
+from urllib.error import HTTPError
 
-import jwt
+import rdflib
 import requests
 import requests.utils
+from flask import request
 from jwcrypto import jwk
-from requests import Request
+import jwcrypto.jwt
+from oic.oic import Client as OicClient
+from oic.utils.authn.client import CLIENT_AUTHN_METHOD
 
+# TODO: This should be in a flask session or in the DB
+STATE_STORAGE = {}
 
 def lookup_provider_from_profile(profile_url: str):
     """
@@ -16,6 +26,7 @@ def lookup_provider_from_profile(profile_url: str):
     """
 
     r = requests.options(profile_url)
+    r.raise_for_status()
     links = r.headers.get('Link')
     if links:
         parsed_links = requests.utils.parse_header_links(links)
@@ -25,17 +36,19 @@ def lookup_provider_from_profile(profile_url: str):
 
     # If we get here, there was no rel in the options. Instead, try and get the card
     # and find its issuer
-    # TODO: https://github.com/solid/webid-oidc-spec/blob/master/application-user-workflow.md#21-retrieve-profile
-    #  says that we can find the OP url at :me -> solid:oidcIssuer
-    #  DW suggests pim:storage, but this is unclear
-    r = requests.get(profile_url,
-                     headers={"Accept": "application/ld+json"})
-
+    graph = rdflib.Graph()
     try:
-        response = r.json()
-        return None
-    except json.decoder.JSONDecodeError:
-        return None
+        graph.parse(profile_url)
+        issuer = rdflib.URIRef("http://www.w3.org/ns/solid/terms#oidcIssuer")
+        triples = list(graph.triples([None, issuer, None]))
+        if triples:
+            # first item in the response, 3rd item in the triple
+            return triples[0][2].toPython()
+    except HTTPError as e:
+        if e.status == 404:
+            print("Cannot find a profile at this url")
+        else:
+            raise e
 
 
 def get_openid_configuration(op_url):
@@ -46,11 +59,13 @@ def get_openid_configuration(op_url):
     """
 
     path = "/.well-known/openid-configuration"
-    url = op_url + path
+    parts = urllib.parse.urlparse(op_url)
+    parts = parts._replace(path=path)
+    url = urllib.parse.urlunparse(parts)
 
     r = requests.get(url, verify=False)
-    configuration = r.json()
-    return configuration
+    r.raise_for_status()
+    return r.json()
 
 
 def load_op_jwks(op_config):
@@ -62,6 +77,7 @@ def load_op_jwks(op_config):
     if "jwks_uri" not in op_config:
         raise ValueError("Cannot find 'jwks_uri'")
     r = requests.get(op_config["jwks_uri"], verify=False)
+    r.raise_for_status()
     return r.json()
 
 
@@ -83,17 +99,56 @@ def dynamic_registration(provider, redirect_url, op_config):
     if "registration_endpoint" not in op_config:
         raise ValueError("Cannot find 'registration_endpoint'")
 
-    data = {
-        "grant_types": ["implicit"],
-        "issuer": provider,
-        "redirect_uris": [redirect_url],
-        "response_types": ["id_token token"],
-        "scope": "openid profile",
-        "application_type": "native"
+    registration_response = OicClient(
+        client_authn_method=CLIENT_AUTHN_METHOD).register(
+            op_config['registration_endpoint'],
+            redirect_uris=[redirect_url])
+    print("Registration response:", registration_response)
+    return registration_response.to_dict()
+
+
+def make_random_string():
+    x = base64.urlsafe_b64encode(os.urandom(40)).decode('utf-8')
+    x = re.sub('[^a-zA-Z0-9]+', '', x)
+    return x
+
+
+def make_verifier_challenge():
+    code_verifier = make_random_string()
+
+    code_challenge = hashlib.sha256(code_verifier.encode('utf-8')).digest()
+    code_challenge = base64.urlsafe_b64encode(code_challenge).decode('utf-8')
+    code_challenge = code_challenge.replace('=', '')
+
+    return code_verifier, code_challenge
+
+
+def generate_authorization_request_external_id(configuration, redirect_url, client_id):
+    auth_url = configuration["authorization_endpoint"]
+
+
+    code_verifier, code_challenge = make_verifier_challenge()
+    state = make_random_string()
+    assert state not in STATE_STORAGE
+    STATE_STORAGE[state] = {
+        'code_verifier': code_verifier,
+        'redirect_url': request.url
     }
 
-    r = requests.post(op_config["registration_endpoint"], json=data, verify=False)
-    return r.json()
+    query = urllib.parse.urlencode({
+        "response_type": "code",
+        "redirect_uri": redirect_url,
+        "code_challenge": code_challenge,
+        "state": state,
+        "code_challenge_method": "S256",
+        "client_id": client_id,
+        # offline_access: also asks for refresh token
+        "scope": "openid webid offline_access",
+    })
+
+    url = auth_url + '?' + query
+    return url
+
 
 
 def generate_authorization_request(configuration, registration, redirect_url, key: jwk.JWK):
@@ -103,6 +158,28 @@ def generate_authorization_request(configuration, registration, redirect_url, ke
     pubkey = key.export_public(as_dict=True)
     pubkey.update({"alg": "RS256", "ext": True, "key_ops": ["verify"]})
 
+    code_verifier, code_challenge = make_verifier_challenge()
+    state = make_random_string()
+    assert state not in STATE_STORAGE
+    STATE_STORAGE[state] = {
+        'code_verifier': code_verifier,
+        'redirect_url': request.url
+    }
+
+    query = urllib.parse.urlencode({
+        "code_challenge": code_challenge,
+        "state": state,
+        "response_type": "code",
+        "redirect_uri": redirect_url,
+        "code_challenge_method": "S256",
+        "client_id": client_id,
+        # offline_access: also asks for refresh token
+        "scope": "openid offline_access",
+    })
+    url = auth_url + '?' + query
+    return url
+
+    """
     nonce = secrets.token_urlsafe(24)
     request = {
         "redirect_uri": redirect_url,
@@ -121,7 +198,7 @@ def generate_authorization_request(configuration, registration, redirect_url, ke
         "scope": "openid",
         "client_id": client_id,
         # TODO: This should be the values we added when registering
-        "response_type": "id_token token",
+        "response_type": "id_token",
         "request": request_jwt,
         "state": state,
     }
@@ -129,3 +206,61 @@ def generate_authorization_request(configuration, registration, redirect_url, ke
     p = Request('GET', auth_url, params=params).prepare()
 
     return p.url
+    """
+
+def make_token_for(keypair, uri, method):
+    jwt = jwcrypto.jwt.JWT(header={
+        "typ":
+        "dpop+jwt",
+        "alg":
+        "ES256",
+        "jwk":
+        keypair.export(private_key=False, as_dict=True)
+    },
+                           claims={
+                               "jti": make_random_string(),
+                               "htm": method,
+                               "htu": uri,
+                               "iat": int(datetime.datetime.now().timestamp())
+                           })
+    jwt.make_signed_token(keypair)
+    return jwt.serialize()
+
+
+def validate_auth_callback(auth_code, state, provider_info, client_id, redirect_uri):
+    assert state in STATE_STORAGE, f"state {state} not in STATE_STORAGE?"
+
+
+    # Generate a key-pair.
+    keypair = jwcrypto.jwk.JWK.generate(kty='EC', crv='P-256')
+
+    code_verifier = STATE_STORAGE[state].pop('code_verifier')
+    print(f"Code verifier: {code_verifier}")
+    print(f"{client_id=}")
+    print(f"{redirect_uri=}")
+    print(f"{auth_code=}")
+    print(f"{provider_info['token_endpoint']=}")
+
+    # Exchange auth code for access token
+    resp = requests.post(url=provider_info['token_endpoint'],
+                         data={
+                             "grant_type": "authorization_code",
+                             "client_id": client_id,
+                             "redirect_uri": redirect_uri,
+                             "code": auth_code,
+                             "code_verifier": code_verifier,
+                         },
+                         headers={
+                             'DPoP':
+                                 make_token_for(
+                                     keypair, provider_info['token_endpoint'],
+                                     'POST')
+                         },
+                         allow_redirects=False)
+    result = resp.json()
+    print("exchange result", result)
+
+    return {
+        "key": keypair.export(),
+        "result": result
+    }
--- solid/webserver.py
+++ solid/webserver.py
@@ -1,6 +1,8 @@
+import jwcrypto
+import jwcrypto.jwt
 import jwt
 import flask
-from flask import request, current_app
+from flask import request, current_app, jsonify
 from flask_login import login_user, login_required, logout_user
 
 import solid
@@ -40,6 +42,30 @@ def create_app():
 
 webserver_bp = flask.Blueprint('register', __name__)
 
+
+@webserver_bp.route("/client_id.jsonld")
+def client_id():
+    # In Solid-OIDC you can register a client by having the "client_id" field be a URL to a json-ld document
+    # It's normally recommended that this is a static file, but for simplicity serve it from flask
+
+    client_information = {
+        "@context": ["https://www.w3.org/ns/solid/oidc-context.jsonld"],
+
+        "client_id": "https://porter.net.nz/~alastair/client_id.jsonld",
+        "client_name": "Alastair's cool test application",
+        "redirect_uris": ["http://localhost:8000/redirect"],
+        "post_logout_redirect_uris": ["http://localhost:8000/logout"],
+        "client_uri": "http://localhost:8000",
+        "logo_uri": "http://localhost:8000/logo.png",
+        "tos_uri": "http://localhost:8000/tos.html",
+        "scope": "openid webid offline_access",
+        "grant_types": ["refresh_token", "authorization_code"],
+        "response_types": ["code"],
+        "default_max_age": 3600,
+        "require_auth_time": true
+    }
+
+    return jsonify(client_information)
 
 @webserver_bp.route("/")
 def web_index():
@@ -95,7 +121,7 @@ def web_register():
     provider_jwks = backend.get_resource_server_keys(provider)
     if provider_config and provider_jwks:
         log_messages.append(f"Configuration for {provider} already exists, skipping setup")
-        print(f"Configuration for {provider} already exists, quitting")
+        print(f"Configuration for {provider} already exists, skipping")
     else:
         provider_config = solid.get_openid_configuration(provider)
         backend.save_resource_server_configuration(provider, provider_config)
@@ -105,31 +131,68 @@ def web_register():
 
         log_messages.append("Got configuration and jwks for provider")
 
-    client_registration = backend.get_client_registration(provider)
-    if client_registration:
-        # TODO: Check if redirect url is the same as the one configured here
-        log_messages.append(f"Registration for {provider} already exists, skipping")
-        print(f"Registration for {provider} already exists, quitting")
-    else:
-        client_registration = solid.dynamic_registration(provider, current_app.config['REDIRECT_URL'], provider_config)
-        backend.save_client_registration(provider, client_registration)
+    # client_registration = backend.get_client_registration(provider)
+    # if client_registration:
+    #     # TODO: Check if redirect url is the same as the one configured here
+    #     log_messages.append(f"Registration for {provider} already exists, skipping")
+    #     print(f"Registration for {provider} already exists, skipping")
+    # else:
+    #     client_registration = solid.dynamic_registration(provider, current_app.config['REDIRECT_URL'], provider_config)
+    #     backend.save_client_registration(provider, client_registration)
+    #
+    #     log_messages.append("Registered client with provider")
 
-        log_messages.append("Registered client with provider")
-
-    auth_url = solid.generate_authorization_request(provider_config,
-                                                    client_registration,
-                                                    current_app.config['REDIRECT_URL'],
-                                                    client_key)
+    # auth_url = solid.generate_authorization_request(provider_config,
+    #                                                 client_registration,
+    #                                                 current_app.config['REDIRECT_URL'],
+    #                                                 client_key)
+    auth_url = solid.generate_authorization_request_external_id(
+        provider_config, current_app.config['REDIRECT_URL'],
+        "https://porter.net.nz/~alastair/client_id.jsonld"
+    )
     log_messages.append("Got an auth url")
+
+    flask.session['provider'] = provider
 
     return flask.render_template("register.html", log_messages=log_messages, auth_url=auth_url)
 
 
 @webserver_bp.route("/redirect")
 def web_redirect():
-    return flask.render_template("redirect.html")
+    auth_code = flask.request.args.get('code')
+    state = flask.request.args.get('state')
+
+    if not auth_code or not state:
+        # No query parameters, it's probably from node-solid-server which uses # hash parameters instead,
+        # use our custom html which captures these and does the post
+        return flask.render_template("redirect_hash.html")
+    else:
+
+        provider = flask.session['provider']
+        provider_config = backend.get_resource_server_configuration(provider)
+        print(provider_config)
+        client_registration = backend.get_client_registration(provider)
+        print(client_registration)
+
+        redirect_uri = current_app.config['REDIRECT_URL']
+        resp = solid.validate_auth_callback(auth_code, state, provider_config, "https://porter.net.nz/~alastair/client_id.jsonld", redirect_uri)
+        result = resp["result"]
+
+        flask.session['key'] = resp["key"]
+        flask.session['access_token'] = result['access_token']
+
+        decoded_access_token = jwcrypto.jwt.JWT()
+        decoded_access_token.deserialize(result['access_token'])
+        decoded_id_token = jwcrypto.jwt.JWT()
+        decoded_id_token.deserialize(result['id_token'])
+        print(f"access token: {decoded_access_token}")
+        print(f"id token: {decoded_id_token}")
+
+        # return flask.redirect(STATE_STORAGE[state].pop('redirect_url'))
+        return flask.render_template("success.html")
 
 
+# This is used by the node-solid-server auth (post)
 @webserver_bp.route("/redirect_post", methods=["POST"])
 def web_redirect_save():
     body = request.json
